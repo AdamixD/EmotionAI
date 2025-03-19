5a54712e9d2c876c9a473d33e7887a665fb4ddd6
adamdabkowski <adamdobrus@gmail.com>
2025-03-19 21:47:11 +0100

    feat: - evaluation.py modified

--- evaluation.py
+++ evaluation.py
@@ -45,9 +45,11 @@ def compute_metrics(p):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model_name", required=True, help="Name of the trained model directory (without 'results_' prefix)")
+    parser.add_argument("-t", "--dataset_type", required=True, help="Type of dataset (train/test")
     args = parser.parse_args()
 
     MODEL_PATH = f"./results_{args.model_name}"
+    DATASET_TYPE = args.dataset_type
     DATA_PATH = "./data/unrefined"
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -58,17 +60,25 @@ if __name__ == "__main__":
     dataset = dataset.cast_column("label", ClassLabel(num_classes=len(label_names), names=label_names))
 
     dataset = dataset.train_test_split(test_size=0.1, stratify_by_column='label', shuffle=True, seed=42)
+    train_dataset = dataset["train"]
     test_dataset = dataset["test"]
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=len(label_names)).to(device)
     model.eval()
 
-    tokenized_test_dataset = test_dataset.map(lambda x: tokenize_function(x, tokenizer), batched=True)
+    if DATASET_TYPE in ("train", "test"):
+        if DATASET_TYPE == "train":
+            tokenized_dataset = train_dataset.map(tokenize_function, batched=True)
+        elif DATASET_TYPE == "test":
+            tokenized_dataset = test_dataset.map(tokenize_function, batched=True)
 
-    trainer = Trainer(model=model)
+        trainer = Trainer(model=model)
 
-    pred_output = trainer.predict(tokenized_test_dataset)
-    metrics = compute_metrics(pred_output)
+        pred_output = trainer.predict(tokenized_dataset)
+        metrics = compute_metrics(pred_output)
 
-    print("Evaluation on test dataset:", metrics)
+        print("Evaluation on test dataset:", metrics)
+
+    else:
+        print("Allowed dataset's types are train or test")
