6a0f446b52f89b24716ac156af03b6de14f0c497
adamdabkowski <adamdobrus@gmail.com>
2025-03-19 21:37:23 +0100

    feat: - evaluation.py added

---
+++ evaluate.py
@@ -0,0 +1,74 @@
+import argparse
+import os
+import numpy as np
+import torch
+
+from datasets import ClassLabel, Dataset
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    Trainer
+)
+
+
+def load_data(data_path):
+    texts, labels, label_names = [], [], []
+    for idx, emotion_dir in enumerate(sorted(os.listdir(data_path))):
+        emotion_path = os.path.join(data_path, emotion_dir, 'gpt-4o')
+        label_names.append(emotion_dir)
+        filename = f"{emotion_dir}.txt"
+        file_path = os.path.join(emotion_path, filename)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            texts += [line.strip() for line in lines if line.strip()]
+            labels += [idx] * len(lines)
+    return texts, labels, label_names
+
+
+def tokenize_function(examples, tokenizer):
+    return tokenizer(examples["text"], padding='max_length', truncation=True, max_length=256)
+
+
+def compute_metrics(p):
+    preds = np.argmax(p.predictions, axis=1)
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        y_true=p.label_ids,
+        y_pred=preds,
+        average='weighted',
+        zero_division=0,
+    )
+    accuracy = accuracy_score(p.label_ids, preds)
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_name", required=True, help="Name of the trained model directory (without 'results_' prefix)")
+    args = parser.parse_args()
+
+    MODEL_PATH = f"./results_{args.model_name}"
+    DATA_PATH = "./data/unrefined"
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    texts, labels, label_names = load_data(DATA_PATH)
+
+    dataset = Dataset.from_dict({'text': texts, 'label': labels})
+    dataset = dataset.cast_column("label", ClassLabel(num_classes=len(label_names), names=label_names))
+
+    dataset = dataset.train_test_split(test_size=0.1, stratify_by_column='label', shuffle=True, seed=42)
+    test_dataset = dataset["test"]
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=len(label_names)).to(device)
+    model.eval()
+
+    tokenized_test_dataset = test_dataset.map(lambda x: tokenize_function(x, tokenizer), batched=True)
+
+    trainer = Trainer(model=model)
+
+    pred_output = trainer.predict(tokenized_test_dataset)
+    metrics = compute_metrics(pred_output)
+
+    print("Evaluation on test dataset:", metrics)
